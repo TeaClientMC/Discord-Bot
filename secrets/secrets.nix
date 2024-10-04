05489d2034e3230f4c7ac2d3211eb1655eb15f90
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-05 05:41:58 +1000

    feat: dev token

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,4 +2,7 @@
 	"token.age".publicKeys = [
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjXW/SwJGY8IjMi9u238xhN7njeruqtDuDeaUy00wPM eveeifyeve@eveeifyeve-macbook"
 	];
+	"devToken.age".publicKeys = [
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjXW/SwJGY8IjMi9u238xhN7njeruqtDuDeaUy00wPM eveeifyeve@eveeifyeve-macbook"
+	];
 }
