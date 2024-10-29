7d9d0d9be6e5dfb6bba8989e25fa5ec42e74fd71
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-29 23:02:56 +1100

    feat: nixos system

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,10 @@
 {
 	"token.age".publicKeys = [
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjXW/SwJGY8IjMi9u238xhN7njeruqtDuDeaUy00wPM eveeifyeve@eveeifyeve-macbook"
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFy81uW8QQKva3vDeKJo0rAoUmzrsTON28zWyWYAh1v eveeifyeve@nixos"
 	];
 	"devToken.age".publicKeys = [
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjXW/SwJGY8IjMi9u238xhN7njeruqtDuDeaUy00wPM eveeifyeve@eveeifyeve-macbook"
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFy81uW8QQKva3vDeKJo0rAoUmzrsTON28zWyWYAh1v eveeifyeve@nixos"
 	];
 }
